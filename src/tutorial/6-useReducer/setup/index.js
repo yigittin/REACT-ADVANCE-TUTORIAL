b6a48b592f9bd5f65ab5df7578e9dfff1d6219a1
yigittin <i.yigittin@hotmail.com>
2021-06-18 22:14:26 +0300

    useReduce

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -4,7 +4,43 @@ import { data } from '../../../data';
 // reducer function
 
 const Index = () => {
-  return <h2>useReducer</h2>;
+  const reducer=(state,action)=>{
+
+  }
+  const defaultState={
+    people:data,
+    isModalOpen:true,
+    modalContent:'hello world',
+  };
+  const [name,setName]=useState('');
+  const [state,dispatch]=useReducer(reducer,defaultState);
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    if(name){
+      
+    }
+    else{
+      
+    }
+  }
+  return (
+    <>
+    {state.isModalOpen&&<Modal modalContent={state.modalContent}/>}
+    <form onSubmit={handleSubmit} className='form'> 
+      <div>
+        <input type="text" value={name} onChange={(e)=>setName(e.target.value)} />
+      </div>
+      <button type='submit'>add</button>
+    </form>
+    {state.people.map((person)=>{
+      return(
+        <div key={person.id}>
+          <h4>{person.name}</h4>
+        </div>
+      )
+    })}
+    </>
+  );
 };
 
 export default Index;
