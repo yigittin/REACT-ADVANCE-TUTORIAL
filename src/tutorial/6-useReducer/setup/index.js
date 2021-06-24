821278f093d323763350e3340780eb62c2a76fc3
yigittin <i.yigittin@hotmail.com>
2021-06-24 18:36:15 +0300

    useReducer done

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -1,19 +1,10 @@
 import React, { useState, useReducer } from 'react';
 import Modal from './Modal';
 import { data } from '../../../data';
-// reducer function
+import {reducer} from './reducer';
 
 const Index = () => {
-  const reducer=(state,action)=>{
-    if(action.type==='ADD_ITEM'){
-      const newPeople=[...state.people,action.payload];
-      return {...state,people:newPeople,isModalOpen:true,modalContent:'item added'};
-    }
-    if(action.type==="NO_VALUE"){
-      return {...state,showModal:true,modalContent:'please enter value'}
-    }
-    throw new Error('no matching action');
-  }
+  
   const defaultState={
     people:[],
     isModalOpen:true,
@@ -31,10 +22,13 @@ const Index = () => {
     else{
       dispatch({type:'NO_VALUE'})
     }
+  };
+  const closeModal=()=>{
+    dispatch({type:'CLOSE_MODAL'});
   }
   return (
     <>
-    {state.isModalOpen&&<Modal modalContent={state.modalContent}/>}
+    {state.isModalOpen&&<Modal closeModal={closeModal} modalContent={state.modalContent}/>}
     <form onSubmit={handleSubmit} className='form'> 
       <div>
         <input type="text" value={name} onChange={(e)=>setName(e.target.value)} />
@@ -43,8 +37,9 @@ const Index = () => {
     </form>
     {state.people.map((person)=>{
       return(
-        <div key={person.id}>
+        <div key={person.id} className='item'>
           <h4>{person.name}</h4>
+          <button onClick={()=>dispatch({type: 'REMOVE_ITEM',payload:person.id})}>Remove</button>
         </div>
       )
     })}
