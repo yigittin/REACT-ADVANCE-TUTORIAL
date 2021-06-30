d71db7ffbbd2c2804b707b90fc978d5132253fa7
yigittin <i.yigittin@hotmail.com>
2021-07-01 01:46:55 +0300

    ContextAPI

--- src/tutorial/8-useContext/setup/1-context-api.js
+++ src/tutorial/8-useContext/setup/1-context-api.js
@@ -3,6 +3,10 @@ import { data } from '../../../data';
 // more components
 // fix - context api, redux (for more complex cases)
 
+const PersonContext=React.createContext();
+
+
+
 const ContextAPI = () => {
   const [people, setPeople] = useState(data);
   const removePerson = (id) => {
@@ -11,22 +15,23 @@ const ContextAPI = () => {
     });
   };
   return (
-    <>
-      <h3>prop drilling</h3>
-      <List people={people} removePerson={removePerson} />
-    </>
+    <PersonContext.Provider value={{removePerson,people}}>      
+      <h3>Context API</h3>
+      <List people={people} />    
+    </PersonContext.Provider>
+   
   );
 };
 
-const List = ({ people, removePerson }) => {
+const List = ({ people}) => {
+  const mainData=useContext(PersonContext);
   return (
     <>
-      {people.map((person) => {
+      {mainData.people.map((person) => {
         return (
           <SinglePerson
             key={person.id}
-            {...person}
-            removePerson={removePerson}
+            {...person}            
           />
         );
       })}
@@ -34,7 +39,9 @@ const List = ({ people, removePerson }) => {
   );
 };
 
-const SinglePerson = ({ id, name, removePerson }) => {
+const SinglePerson = ({ id, name}) => {
+  const {removePerson}=useContext(PersonContext);
+
   return (
     <div className='item'>
       <h4>{name}</h4>
